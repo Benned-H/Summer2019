82c0cd53da593cd89a9acbe7b74c0ae957522b0d
Benned-H <benned1@gmail.com>
2019-06-19 21:58:12 -0400

    Camera setup done

--- camera.py
+++ camera.py
@@ -1,6 +1,18 @@
 """
 Testing with the Raspberry Pi camera.
-Benned Hedegaard
-5/12/2019
+Author: Benned Hedegaard
+Last revised: 6/19/2019
 """
-import 
+from picamera import PiCamera
+from time import sleep # Lets us create time delays
+
+camera = PiCamera() # Instance of the main class of picamera.
+print(camera.resolution)
+
+camera.rotation = 180 # Rotates image so it's right-side up.
+camera.hflip = True # Horizontal flip.
+camera.start_preview(alpha=225) # alpha adjusts transparency 0-255
+sleep(5)
+camera.capture('./image.jpg')
+camera.stop_preview()
+camera.close()
