324e465ccde43d4bde95f70f7830b62891083fe5
Benned-H <benned1@gmail.com>
2019-06-28 02:40:50 -0400

    Face recognition time

---
+++ PythonProjects/FaceRecognizer.py
@@ -0,0 +1,8 @@
+"""
+Coded with guide from Adrian Rosebrock:
+Raspberry Pi Face Recognition
+
+Author: Benned Hedegaard
+Last revised 6/28/2019
+"""
+# Need to install OpenCV/CMake first apparently.
