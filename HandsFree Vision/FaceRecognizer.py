9cb5b30a46209b5b0fdc8dc079781d99f40d9b0c
Benned Hedegaard <benned1@gmail.com>
2019-07-14 23:58:28 -0500

    Delete FaceRecognizer.py

--- HandsFree Vision/FaceRecognizer.py
+++
@@ -1,8 +0,0 @@
-"""
-Coded with guide from Adrian Rosebrock:
-Raspberry Pi Face Recognition
-
-Author: Benned Hedegaard
-Last revised 6/28/2019
-"""
-# Need to install OpenCV/CMake first apparently.
