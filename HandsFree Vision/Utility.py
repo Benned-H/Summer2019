aae818774c26f569fd904900173e9ffd759abca4
Benned-H <benned1@gmail.com>
2019-07-17 01:31:54 -0500

    UI code cont.

--- HandsFree Vision/Utility.py
+++ HandsFree Vision/Utility.py
@@ -3,7 +3,7 @@ All the miscellaneous functions needed
 for the entire hand segmentation workflow.
 
 Author: Benned Hedegaard
-Last Revised: 7/15/2019
+Last Revised: 7/17/2019
 """
 from picamera import PiCamera
 from time import sleep
@@ -25,6 +25,14 @@ def preview(camera, seconds=2):
     camera.start_preview(alpha=225) # alpha adjusts transparency 0-255
     sleep(seconds)
     camera.stop_preview()
+    
+def previewText(camera,text,seconds=2):
+    save_text = camera.annotate_text
+    camera.annotate_text = text
+    camera.start_preview(alpha=225) # alpha adjusts transparency 0-255
+    sleep(seconds)
+    camera.stop_preview()
+    camera.annotate_text = save_text
 
 def addBox(mask,r,c,h,w):
     """Adds a red box to the given mask at [r,c]
@@ -39,27 +47,30 @@ def addBox(mask,r,c,h,w):
         print("Invalid input: Column out of bounds.")
         return mask
     
-    pixel_val = [255,0,0]
+    pixel_val = [255,0,0,255]
     print(mask.shape)
     mask[r,c:c+w] = pixel_val # Top side.
-    #mask[r+1,c:c+w] = pixel_val
+    mask[r+1,c:c+w] = pixel_val
     mask[r+h,c:c+w] = pixel_val # Bottom side.
-    #mask[r+h-1,c:c+w] = pixel_val
+    mask[r+h-1,c:c+w] = pixel_val
     mask[r:r+h,c] = pixel_val # Left side.
-    #mask[r:r+h,c+1] = pixel_val
+    mask[r:r+h,c+1] = pixel_val
     mask[r:r+h,c+w] = pixel_val # Right side.
-    #mask[r:r+h,c+w-1] = pixel_val
+    mask[r:r+h,c+w-1] = pixel_val
     return mask
-    
-def createMask(camera,r,c,h,w):
-    res = camera.resolution
-    mask = np.zeros((res[0],res[1],3), dtype=np.uint8)
+
+def createMask(camera,r,c,h,w,seconds=2):
+    res = camera.resolution # This will be columns by rows.
+    mask = np.zeros((res[1],res[0],4), dtype=np.uint8)
     mask = addBox(mask,r,c,h,w)
     mask_img = PIL.Image.fromarray(mask)
-    
+    # mask_img.save("test.png")
+    return mask_img
+
+def showMask(camera,mask_img,seconds=2):
     camera.start_preview(alpha=225)
-    o = camera.add_overlay(mask_img.tobytes(), size = res,
-                           layer = 3, alpha = 60)
-    sleep(10)
+    o = camera.add_overlay(mask_img.tobytes(), size = mask_img.size,
+                           layer = 3, alpha = 10)
+    sleep(seconds)
     camera.stop_preview()
     camera.remove_overlay(o)
--- HandsFree Vision/setup.py
+++ HandsFree Vision/setup.py
@@ -1,14 +1,17 @@
 """
 The main method for the hand segmentation process.
 Author: Benned Hedegaard
-Last revised: 7/15/2019
+Last revised: 7/17/2019
 """
 import Utility as util
 
 def main():
     camera = util.setupCamera()
-    bb_h = 80 # Bounding box height, width
+    bb_h = 100 # Bounding box height, width
     bb_w = 100
+    bb_r = 0
+    bb_c = 0
+    mask = None
     
     while (True):
         instr = input("What should we do?\n* 1 - Adjust bounding boxes\n* 2 - Adjust resolution\n* 3 - Ready\n* q - Quit program\n")
@@ -19,8 +22,12 @@ def main():
         # Other cases:
         if instr == "1":
             # Adjust the bounding boxes.
-            util.createMask(camera,0,0,bb_h,bb_w)
-            util.preview(camera)
+            util.previewText(camera,"Here's the current bounding box:",1)
+            if mask == None:
+                print("good")
+                mask = util.createMask(camera,bb_r,bb_c,bb_h,bb_w)
+            util.showMask(camera,mask)
+            input("Should we adjust this placement or size? (y/n) \n")
             continue
 
 if __name__ == "__main__":
